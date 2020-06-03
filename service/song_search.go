5450f9499e5b786ed0398f4596b55d0d9eba9d41
Artem Torubarov <artem.torubarov@t-systems.com>
2020-06-03 23:44:03 +0300

    search song

--- service/song_search.go
+++ service/song_search.go
@@ -59,7 +59,7 @@ func (s *songSearch) handleCallback(update bot.Update) {
 		return
 	}
 	if strings.Contains(err.Error(), res.TxtAddSongNoDj) || strings.HasSuffix(err.Error(), "DJ can use /settings command to increase max songs number.") {
-		_, err = s.bot.Send(bot.NewMessage(update.Message.Chat.ID, err.Error()))
+		_, err = s.bot.Send(bot.NewMessage(update.CallbackQuery.Message.Chat.ID, err.Error()))
 		if err != nil {
 			logrus.WithError(err).Error("Unable handle search callback")
 		}
